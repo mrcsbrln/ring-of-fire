53b0635e4ec6cb0148cb307de9ea9af132f359fb
mrcsbln <hrtmnn.mrcs@gmail.com>
2024-11-12 21:56:22 +0100

    implemented game-info-card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,13 +8,14 @@ import { PlayerComponent } from "../player/player.component";
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { GameInfoComponent } from "../game-info/game-info.component";
 
 
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, GameInfoComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
